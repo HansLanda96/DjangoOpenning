29787b0406672dfb47aad748718b2843bdbec265
Iegor <duuurrr17@gmail.com>
2022-09-30 16:46:47 +0300

    change clean func for student form

--- students/forms.py
+++ students/forms.py
@@ -1,8 +1,7 @@
-import re
-
 from django import forms
 
 from .models import Student
+from .utils import validate_phone_number
 
 
 class CreateStudentForm(forms.ModelForm):
@@ -31,24 +30,7 @@ class CreateStudentForm(forms.ModelForm):
         return self.cleaned_data.get('last_name').lower().capitalize()
 
     def clean_phone_number(self):
-        phone_without_symbols = re.sub(r'\D', '', self.cleaned_data.get('phone_number'))
-        if 13 > len(phone_without_symbols) >= 10:
-            format_phone = re.sub(
-                r"^(.*?)(0)(39|50|66|95|99|67|68|89|91|92|94|96|97|98|[679]3)(\d{3})(\d{2})(\d{2})$",
-                r"+38\2(\3)-\4-\5-\6",
-                phone_without_symbols
-            )
-            if format_phone == phone_without_symbols:
-                raise forms.ValidationError(
-                    'Phone number is not match any Ukrainian cellular operator'
-                )
-            else:
-                return format_phone
-        else:
-            if len(phone_without_symbols) >= 13:
-                raise forms.ValidationError('Phone number is too long >= 13 digits')
-            else:
-                raise forms.ValidationError('Phone number is too short (Less than 10 digits)')
+        return validate_phone_number(self.cleaned_data.get('phone_number'))
 
 
 class EditStudentForm(CreateStudentForm):
