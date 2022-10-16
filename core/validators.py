fa44e3e67a73eeb7351df96706d1b294c2d21f8f
Iegor <duuurrr17@gmail.com>
2022-10-17 01:22:28 +0300

    Rebase validator
    Rebase from Student app to Core app

--- core/validators.py
+++ core/validators.py
@@ -1,22 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
-# Functional validator for Students model email field
-# def validate_email_domains(value):
-#     valid_domains = ['@gmail.com', '@yahoo.com']
-#     for domain in valid_domains:
-#         if domain in value:
-#             break
-#     else:
-#         raise ValidationError('Invalid email domain')
-
-
-# disable this validator for now
-# def unique_email_validator(value):
-#     from .models import Student
-#     if Student.objects.filter(email=value).exists():
-#         raise ValidationError(f'Student with email {value} already exists. Try to Log In.')
-
 
 @deconstructible
 class ValidEmailDomain:
