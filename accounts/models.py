1a8ed9f4f56935668d6b303efe157988dbfa64e7
Iegor <duuurrr17@gmail.com>
2022-10-25 00:13:53 +0300

    Insert all changes for accounts app from class 22

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
-from django.db import models   # noqa
+from django.contrib.auth.models import User
+from django.db import models
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta:
+        db_table = 'lms_profiles'
+
+
+@receiver(models.signals.post_save, sender=User)
+def create_new_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
