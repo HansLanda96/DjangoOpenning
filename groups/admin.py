d19090564f44c17f16327def5fbc74e673c8abbd
Iegor <duuurrr17@gmail.com>
2022-10-26 01:12:27 +0300

    Register apps in admin panel
    Groups and Teachers

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin    # noqa
+from django.contrib import admin
 
-# Register your models here.
+
+from .models import Group
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'headman_info', 'course_fix', 'start_date', 'end_date', 'students_count')
+    list_display_links = list_display
+    list_per_page = 15
+    list_filter = ['course', 'start_date']
+    empty_value_display = '-not stated-'
+
+    fieldsets = (
+        (None, {'fields': ('name', 'course', 'headman')}),
+    )
+
+    def headman_info(self, instance):
+        if instance.headman:
+            return f'{instance.headman.last_name} {instance.headman.first_name}'
+        return self.empty_value_display
+    headman_info.short_description = 'headman'
+
+    def students_count(self, instance):
+        return instance.students.count()
+    students_count.short_description = 'students count'
+
+    def course_fix(self, instance):
+        if instance.course:
+            return instance.course.title()
+        return self.empty_value_display
+    course_fix.short_description = 'course'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):    # choice headman from students in group
+        if db_field.name == 'headman':
+            kwargs['queryset'] = Group.objects.get(pk=request.resolver_match.kwargs['object_id']).students.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Group, GroupAdmin)
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
 
-# Register your models here.
+
+from .models import Teacher
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'specialization', 'format_salary')
+    list_display_links = list_display
+    list_per_page = 15
+    list_filter = ['specialization']
+    ordering = ['last_name']
+
+    fieldsets = (
+        ('Personal info', {'fields': ('last_name', 'first_name')}),
+        ('Birthday', {'fields': ('birthday', 'age')}),
+        ('Contacts', {'fields': ('email', 'phone_number')}),
+        ('Additional info', {'fields': ('specialization', 'salary')}),
+    )
+
+    def age(self, instance):
+        if instance.get_age():
+            return f'{instance.get_age()} y.o.'
+        return 'Age is not specified'
+    readonly_fields = ('age',)
+    age.short_description = 'age'
+
+    def format_salary(self, instance):
+        return "{:,} $".format(instance.salary)
+    format_salary.short_description = 'salary'
+
+
+admin.site.register(Teacher, TeacherAdmin)
