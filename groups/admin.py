cd08521fe9c8112d2c2342c141ac9a2234e1544e
Iegor <duuurrr17@gmail.com>
2022-11-05 00:13:29 +0200

    Modify admin.py in Groups
    Final optimization for group admin. Includes some changes in functions

--- groups/admin.py
+++ groups/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from .models import Group
 
@@ -10,6 +13,12 @@ class StudetnInlineTable(admin.TabularInline):
     extra = 0
     readonly_fields = ('first_name', 'last_name')
     show_change_link = True
+
+    def get_queryset(self, request):
+        queryset = self.model.objects.filter(
+            group_id=int(request.resolver_match.kwargs['object_id'])
+        ).select_related('group')
+        return queryset
 
     def has_delete_permission(self, request, obj=None) -> bool:
         return False
@@ -74,8 +83,12 @@ class GroupAdmin(admin.ModelAdmin):
     headman_info.short_description = 'headman'
 
     def students_count(self, instance):
-        return instance.students.count()
-    students_count.short_description = 'students count'
+        count = instance.students.count()
+        url = (
+            reverse("admin:students_student_changelist") + '?' + urlencode({'group_id': f'{instance.pk}'})
+        )
+        return format_html('<a href="{}">{} student(s)</a>', url, count)
+    students_count.short_description = 'Students'
 
     def course_fix(self, instance):
         if instance.course:
@@ -83,16 +96,17 @@ class GroupAdmin(admin.ModelAdmin):
         return self.empty_value_display
     course_fix.short_description = 'course'
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):  # choice headman from students in group
-        if db_field.name == 'headman':
-            kwargs['queryset'] = Group.objects.get(pk=request.resolver_match.kwargs['object_id']).students.all()
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs):  # choice headman from students in group
+    #     if db_field.name == 'headman':
+    #         kwargs['queryset'] = Group.objects.get(pk=request.resolver_match.kwargs['object_id']).students.all()
+    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
         form.base_fields['headman'].widget.can_add_related = False
         form.base_fields['headman'].widget.can_change_related = False
         form.base_fields['headman'].widget.can_delete_related = False
+        form.base_fields['headman'].queryset = obj.students.all()   # same as def formfield_for_foreignkey
         return form
 
     inlines = [StudetnInlineTable, TeacherInlineTable, ]
